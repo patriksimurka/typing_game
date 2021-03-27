5e5fed3c04985d19028f54837d74aae1c2272a53
patriksimurka <patriksimurka@gmail.com>
2021-03-27 18:05:07 +0100

    wpm

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const quote_url = 'http://api.quotable.io/random'
 const quote_display = document.getElementById('quote')
+let written = []
 
 function get_quote() {
 	return fetch(quote_url).then(response => response.json()).then(data => data.content)
+}
+
+function get_wpm() {
+	elapsed = (new Date() - start_time) / 1000 / 60
+	let arr_quote = quote_display.querySelectorAll('span')
+	let count = 0
+	arr_quote.forEach((span) => {
+		if (span.classList.contains('correct')) {
+			count++
+		}
+	})
+	document.getElementById('wpm').innerText = Math.floor((count/4.7)/(elapsed)) + ' WPM'
 }
 
 async function get_next_quote() {
@@ -13,11 +26,38 @@ async function get_next_quote() {
 		character_span.innerText = character
 		quote_display.appendChild(character_span)
 	})
+	
 }
 
+let start_time
 document.body.addEventListener("keypress", function(event) {
-    console.log(event.keyCode)
+	let letter = String.fromCharCode(event.keyCode)
+	written.push(letter)
+    let arr_quote = quote_display.querySelectorAll('span')
+	written.forEach((character, index) => {
+		if (character === arr_quote[index].innerText) {
+			arr_quote[index].classList.add('correct')
+		} else {
+			arr_quote[index].classList.add('incorrect')
+		}
+	})
+	if (start_time == null) {
+	start_time = new Date()
+	setInterval(() => {
+		get_wpm()
+	}, 200)
+	}
 })
 
+document.body.addEventListener("keydown", function(event) {
+	if (event.keyCode === 8) {
+		let index = written.length - 1
+		written.pop()
+		let arr_quote = quote_display.querySelectorAll('span')
+		arr_quote[index].classList.remove('incorrect')
+		arr_quote[index].classList.remove('correct')
+	}
+})
 
 get_next_quote()
+
